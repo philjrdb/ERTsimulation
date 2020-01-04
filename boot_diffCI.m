dd6c77911a1222a933731a0e7a93f7c2331d7420
philjrdb <57160185+philjrdb@users.noreply.github.com>
2020-01-04 17:30:09 +1100

    Update boot_diffCI.m

--- boot_diffCI.m
+++ boot_diffCI.m
@@ -9,6 +9,22 @@ function bootCI = boot_diffCI(data1,data2,num_boots,sig)
 %%Output:
 % bootCI = LCI+UCI vector
 
+%  Copyright 2019 Philip Jean-Richard-dit-Bressel, UNSW Sydney
+%  Based on Colin Clifford 2018 bootstrap_CI.m 
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 [num_trials1,window] = size(data1);
 [num_trials2,~] = size(data2);
 
