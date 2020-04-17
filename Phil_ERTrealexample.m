d4d5ba78e8a8df5dc6b5e6d3e40a28a0c515c626
philjrdb <57160185+philjrdb@users.noreply.github.com>
2020-04-17 15:09:27 +1000

    Add files via upload

--- Phil_ERTrealexample.m
+++ Phil_ERTrealexample.m
@@ -5,7 +5,7 @@ consec_thresh = 340; % 1017.3Hz sample rate / 3Hz filter
 % Graphing parameters
 ylims = [-1 4];
 xlims = [-2 5];
-sig_level = linspace(3.5,4,7);
+sig_plot_level = linspace(4,3.2,7);
 savefolder = ...
    'M:\Gavan McNally''s Lab\Staff folders\Philip';
 
@@ -37,41 +37,45 @@ diff_bCI = boot_diffCI(ERT_test.Cp_off1,ERT_test.Cm_off3,1000,sig);
 diff_bCIexp = [adjLCI;adjUCI];
 
 %% Significance bars
-Cp_bCIexp_sig = NaN(1,ev_win);
-sig_idx = find((Cp_bCIexp(1,:) > 0) | (Cp_bCIexp(2,:) < 0));
-consec = consec_idx(sig_idx,consec_thresh);
-Cp_bCIexp_sig(sig_idx(consec)) = sig_level(3);
-
-Cm_bCIexp_sig = NaN(1,ev_win);
-sig_idx = find((Cm_bCIexp(1,:) > 0) | (Cm_bCIexp(2,:) < 0));
-consec = consec_idx(sig_idx,consec_thresh);
-Cm_bCIexp_sig(sig_idx(consec)) = sig_level(4);
-
+%tCI
 Cp_tCI_sig = NaN(1,ev_win);
 sig_idx = find((Cp_tCI(1,:) > 0) | (Cp_tCI(2,:) < 0));
 consec = consec_idx(sig_idx,consec_thresh);
-Cp_tCI_sig(sig_idx(consec)) = sig_level(5);
+Cp_tCI_sig(sig_idx(consec)) = sig_plot_level(1);
 
 Cm_tCI_sig = NaN(1,ev_win);
 sig_idx = find((Cm_tCI(1,:) > 0) | (Cm_tCI(2,:) < 0));
 consec = consec_idx(sig_idx,consec_thresh);
-Cm_tCI_sig(sig_idx(consec)) = sig_level(6);
-
-perm_p_sig = NaN(1,ev_win);
-sig_idx = find(perm_p < sig);
-consec = consec_idx(sig_idx,consec_thresh);
-perm_p_sig(sig_idx(consec)) = sig_level(1);
-
-diff_bCIexp_sig = NaN(1,ev_win);
-sig_idx = find((diff_bCIexp(1,:) > 0) | (diff_bCIexp(2,:) < 0));
-consec = consec_idx(sig_idx,consec_thresh);
-diff_bCIexp_sig(sig_idx(consec)) = sig_level(2);
+Cm_tCI_sig(sig_idx(consec)) = sig_plot_level(2);
 
 diff_tCI_sig = NaN(1,ev_win);
 sig_idx = ttest2(ERT_test.Cp_off1,ERT_test.Cm_off3);
 sig_idx = find(sig_idx == 1);
 consec = consec_idx(sig_idx,consec_thresh);
-diff_tCI_sig(sig_idx(consec)) = sig_level(7);
+diff_tCI_sig(sig_idx(consec)) = sig_plot_level(3);
+
+%bCI
+Cp_bCIexp_sig = NaN(1,ev_win);
+sig_idx = find((Cp_bCIexp(1,:) > 0) | (Cp_bCIexp(2,:) < 0));
+consec = consec_idx(sig_idx,consec_thresh);
+Cp_bCIexp_sig(sig_idx(consec)) = sig_plot_level(4);
+
+Cm_bCIexp_sig = NaN(1,ev_win);
+sig_idx = find((Cm_bCIexp(1,:) > 0) | (Cm_bCIexp(2,:) < 0));
+consec = consec_idx(sig_idx,consec_thresh);
+Cm_bCIexp_sig(sig_idx(consec)) = sig_plot_level(5);
+
+diff_bCIexp_sig = NaN(1,ev_win);
+sig_idx = find((diff_bCIexp(1,:) > 0) | (diff_bCIexp(2,:) < 0));
+consec = consec_idx(sig_idx,consec_thresh);
+diff_bCIexp_sig(sig_idx(consec)) = sig_plot_level(6);
+
+%Permutation test
+perm_p_sig = NaN(1,ev_win);
+sig_idx = find(perm_p < sig);
+consec = consec_idx(sig_idx,consec_thresh);
+perm_p_sig(sig_idx(consec)) = sig_plot_level(7);
+
 
 %% Plot
 figure; hold on
@@ -81,20 +85,25 @@ errorplot3(mean_Cm-sem_Cm,mean_Cm+sem_Cm,[-4 5],col_rep(3),.15)
 plot(timeline,mean_Cp,'Color',col_rep(2))
 errorplot3(mean_Cp-sem_Cp,mean_Cp+sem_Cp,[-4 5],col_rep(2),.15)
 
-plot(timeline,Cp_bCIexp_sig,'Color',col_rep(2),'Marker','.')
-text(xlims(1),sig_level(3),'CS+ bCI','Color',col_rep(2));
-plot(timeline,Cm_bCIexp_sig,'Color',col_rep(3),'Marker','.')
-text(xlims(1),sig_level(4),'CS- bCI','Color',col_rep(3));
+%Plor tCI sig
 plot(timeline,Cp_tCI_sig,'Color',col_rep(2),'Marker','.')
-text(xlims(1),sig_level(5),'CS+ tCI','Color',col_rep(2));
+text(xlims(1),sig_plot_level(1),'\bf CS+ tCI','Color',col_rep(2));
 plot(timeline,Cm_tCI_sig,'Color',col_rep(3),'Marker','.')
-text(xlims(1),sig_level(6),'CS- tCI','Color',col_rep(3));
-plot(timeline,perm_p_sig,'Color',col_rep(1),'Marker','.')
-text(xlims(1),sig_level(1),'Perm','Color',col_rep(1));
-plot(timeline,diff_bCIexp_sig,'Color',col_rep(4),'Marker','.')
-text(xlims(1),sig_level(2),'Diff bCI','Color',col_rep(4));
+text(xlims(1),sig_plot_level(2),'\bf CS- tCI','Color',col_rep(3));
 plot(timeline,diff_tCI_sig,'Color',col_rep(4),'Marker','.')
-text(xlims(1),sig_level(7),'Diff tCI','Color',col_rep(4));
+text(xlims(1),sig_plot_level(3),'\bf Diff tCI','Color',col_rep(4));
+
+%Plot bCI sig
+plot(timeline,Cp_bCIexp_sig,'Color',col_rep(2),'Marker','.')
+text(xlims(1),sig_plot_level(4),'\bf CS+ bCI','Color',col_rep(2));
+plot(timeline,Cm_bCIexp_sig,'Color',col_rep(3),'Marker','.')
+text(xlims(1),sig_plot_level(5),'\bf CS- bCI','Color',col_rep(3));
+plot(timeline,diff_bCIexp_sig,'Color',col_rep(4),'Marker','.')
+text(xlims(1),sig_plot_level(6),'\bf Diff bCI','Color',col_rep(4));
+
+%Plot permutation test sig
+plot(timeline,perm_p_sig,'Color',col_rep(1),'Marker','.')
+text(xlims(1),sig_plot_level(7),'\bf Perm','Color',col_rep(1));
 
 plot([-0.5 -0.5],ylim,'k:')
 plot(xlim,[0 0],'k--')
