1677564129a08998038e98c8a4814c33ce80eb16
philjrdb <57160185+philjrdb@users.noreply.github.com>
2024-04-24 18:14:44 +1000

    Update CIadjust.m
    Bug fix:
    - Amended incorrect operation when calculating adjusted LCI for adjustment type 1 (extending CI from reference points)
    
    Other:
    - Minor improvements to comments and reporting

--- CIadjust.m
+++ CIadjust.m
@@ -1,6 +1,6 @@
 function [adjLCI,adjUCI] = CIadjust(LCI,UCI,est,n,adj_type)
 %% Adjusts CI according to adj_type
-   % Type 1 = extend CI from mean by sqrt((n)/(n-1))
+   % Type 1 = extend CI from reference by sqrt((n)/(n-1))
       % needs all inputs
    % Type 2 = expand CI by sqrt((n)/(n-1))
       % doesn't need mean
@@ -8,7 +8,7 @@ function [adjLCI,adjUCI] = CIadjust(LCI,UCI,est,n,adj_type)
 %%Inputs:
 % LCI = lower confidence limit vector
 % UCI = upper confidence limit vector
-% est = parameter estimate  vector (eg. mean, only necessary for Type 1)
+% est = parameter estimate  vector (eg. mean, median) [only needed for adjustment type 1]
 % n = experimental n
 % adj_type = adjustment type (see above)
 
@@ -16,10 +16,9 @@ function [adjLCI,adjUCI] = CIadjust(LCI,UCI,est,n,adj_type)
 % adjLCI = adjusted LCI
 % adjUCI = adjusted UCI
 
-%% By Philip Jean-Richard-dit-Bressel, UNSW Sydney, 2020
-% Feel free to use with citation: Jean-Richard-dit-Bressel et al. (2020). https://doi.org/10.3389/fnmol.2020.00014
-
 %% GNU
+%  Copyright 2020 Philip Jean-Richard-dit-Bressel, UNSW Sydney
+
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
@@ -34,17 +33,16 @@ function [adjLCI,adjUCI] = CIadjust(LCI,UCI,est,n,adj_type)
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 %%
+CI_fix = sqrt((n)/(n-1));
 if adj_type == 1
-   %% Extend CI from mean
-   CI_fix = sqrt((n)/(n-1));
-   fprintf(['CI extended from mean by ' num2str(CI_fix*100) 'pc\n']);
+   %% Extend CI from reference
+   fprintf(['CI extended from reference by ' num2str(CI_fix*100) 'pct\n']);
    adjUCI = (UCI-est).*CI_fix + est;
-   adjLCI = est - (est-LCI)./CI_fix; 
+   adjLCI = est - (est-LCI).*CI_fix; 
    
 elseif adj_type == 2
    %% Expand CI
-   CI_fix = sqrt((n)/(n-1));
-   fprintf(['CI expanded by ' num2str(CI_fix*100) 'pc\n']);
+   fprintf(['CI expanded by ' num2str(CI_fix*100) 'pct\n']);
    CIchange = ((UCI - LCI).*CI_fix - (UCI - LCI))/2; %/2
    adjUCI = UCI+CIchange;
    adjLCI = LCI-CIchange;
